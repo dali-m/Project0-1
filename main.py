697ed9ad6b18a2328c4161d08b339e1c9fe4c993
Dalilah Ali <dali11@hawkmail.hccfl.edu>
2024-10-26 14:01:33 -0400

    Added Menu

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 #CarFinder
 #This program allows a user to find allowed vehicles.
 
-AllowedVehiclesList = ['Ford F-150','Chevrolet Silverado','Tesla Cybertruck','Toyota Tundra','Nissan Titan']
+AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla Cybertruck', 'Toyota Tundra', 'Nissan Titan']
+
+#Adding the menu.
+def print_menu():
+    menu = f"""
+********************************
+AutoCountry Vehicle Finder v0.1
+********************************
+Please Enter the following number below from the following menu:
+
+1. Print all Authorized Vehicles
+2. Exit
+"""
+    print(menu)
